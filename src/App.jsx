ee838b01eec462c440333f501974f78e8455cf64
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-01-20 12:59:31 +0100

    customize ui and removing initial unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+    <main className="App">
+      <header>
+        <a href="./">
+          <img src="/tic-tac-toe.svg" className="logo" alt="Tic-Tac-Toe logo" />
+          <h1>Tic-Tac-Toe</h1>
         </a>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      </header>
+    </main>
   )
 }
 
