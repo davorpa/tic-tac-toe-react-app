88e31db58c50fed70d51ca5575c775a00835f21a
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-01-20 18:08:55 +0100

    ui: design brand heading

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ function App() {
   return (
     <main className="App">
       <header>
-        <a href="./">
+        <a href="./" className="branding">
           <img src="/tic-tac-toe.svg" className="logo" alt="Tic-Tac-Toe logo" />
-          <h1>Tic-Tac-Toe</h1>
+          <h1 className="title">
+            <span>Tic</span>-<span>Tac</span>-<span>Toe</span>
+          </h1>
         </a>
       </header>
     </main>
