88d263e6bbbe8a9495a66f5f47ba12325c406286
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-01-21 20:33:24 +0100

    share PUBLIC_URL between local dev & GHP prod enviroment
    Docs: https://es.vitejs.dev/guide/static-deploy.html#github-pages

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
     <main className="App">
       <header>
         <a href="./" className="branding">
-          <img src="/tic-tac-toe.svg" className="logo" alt="Tic-Tac-Toe logo" />
+          <img src="tic-tac-toe.svg" className="logo" alt="Tic-Tac-Toe logo" />
           <h1 className="title">
             <span>Tic</span>-<span>Tac</span>-<span>Toe</span>
           </h1>
