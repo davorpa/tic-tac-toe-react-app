081bcabe7bd47d131777e584f9713cd919dd581b
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-02-22 04:09:53 +0100

    config: update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,28 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const USE_REPO_PAGES = !process.env.VITE_ROOT_PAGES
-const REPO_NAME = path.basename(process.cwd())
-const mode =
-  process.env.NODE_ENV === 'production' ? 'production' : 'development'
-const base = mode === 'production' && USE_REPO_PAGES ? `/${REPO_NAME}/` : '/'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base,
-  mode,
-  plugins: [react()]
-})
+/** @type {import('vite').UserConfig} */
+export default ({ command, mode }) => {
+  const root = process.cwd()
+  const folderName = path.basename(root)
+  // append folder name to base path if using GitHub/GitLab repo pages
+  const usingRepoPages = !process.env.VITE_ROOT_PAGES
+  const base = mode === 'production' && usingRepoPages ? `/${folderName}/` : '/'
+
+  console.log('Configuring with...', {
+    command,
+    mode,
+    usingRepoPages,
+    root,
+    folderName,
+    base
+  })
+
+  return defineConfig({
+    root,
+    base,
+    mode,
+    plugins: [react()]
+  })
+}
